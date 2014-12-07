5109ccb751177e777c3f01a1af6022eeb79c6ed6
Coolway99 <xxcoolwayxx@gmail.com>
2014-12-07 07:27:25 -0500

    Added threads to handle updating and sequences

--- LD31/src/main/Main.java
+++ LD31/src/main/Main.java
@@ -14,6 +14,19 @@ public class Main extends JFrame{
 	public static final Random ran = new Random();
 	public static final MainPanel mainPanel = new MainPanel();
 	public static final InputListener inputListener = new InputListener();
+	public static final Timer update = new Timer("Update Thread", false);
+	public static final MainMenu menu = new MainMenu("Main Menu");
+	public static final TimerTask updateTask = new TimerTask(){
+		@Override 
+		public void run(){
+			Main.update();
+		}
+		@Override
+		public boolean cancel(){
+			Main.menu.Menu = true;
+			return super.cancel();
+		}
+	};
 
 	public Main(String s){
 		super(s);
@@ -28,16 +41,41 @@ public class Main extends JFrame{
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.setResizable(false);
-		Timer update = new Timer("Update Thread", false);
-		update.scheduleAtFixedRate(new TimerTask(){
-			@Override
-			public void run(){
-				Main.update();
-			}
-		}, 33, 33);
-	}
+		update.scheduleAtFixedRate(updateTask, 33, 33);
+		}
 	public static void update(){
 		mainPanel.update(inputListener.keycode, inputListener.mospos);
 		mainWindow.repaint();
 	}
+	class Sequences {
+		private boolean[] teirList = new boolean [5];
+		private long ticks = 0L;
+		public void enableTeir(int teir){
+			if(teir < teirList.length){
+				teirList[teir] = true;
+			}
+		}
+		public void update(){
+			ticks++;
+			
+		}
+	}
+}
+class MainMenu extends Thread{
+	public boolean Menu = true;
+	
+	public MainMenu(String s){
+		super(s);
+	}
+	@Override
+	public void run() {
+		while(true){
+			while(!Menu){
+				try{
+					Thread.sleep(100);
+				} catch(InterruptedException e){}
+			}
+			
+		}
+	};
 }
