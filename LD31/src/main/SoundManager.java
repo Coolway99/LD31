081444eff16a8fb89d18c38b8f83be1811bfa856
Coolway99 <xxcoolwayxx@gmail.com>
2014-12-07 09:36:10 -0500

    Move SoundThread to seperate file

--- LD31/src/main/SoundManager.java
+++ LD31/src/main/SoundManager.java
@@ -1,46 +1,13 @@
 package main;
 
-import java.io.IOException;
-
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundManager {
 	public static SoundThread enemy, bullet, explosion, menu, explosionRecover;
 	public static void init(){
 		enemy = new SoundThread("assets/enemySpawn.wav");
 		bullet = new SoundThread("assets/bullet.wav");
-		explosion = new SoundThread("assets/explsion.wav");
+		explosion = new SoundThread("assets/explosion.wav");
 		menu = new SoundThread("assets/menu.wav");
 		explosionRecover = new SoundThread("assets/explosionrecover.wav");
-	}
-}
-class SoundThread extends Thread{
-	Clip clip;
-	public SoundThread(String url){
-		try {
-			clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(Main.class.getClassLoader().getResourceAsStream(url)));
-		} catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		}
-	}
-	private SoundThread(Clip clip){
-		this.clip = clip;
-	}
-	@Override
-	public void run(){
-		clip.setFramePosition(0);
-		clip.start();
-	}
-	public void playClip(){
-		clone().start();
-	}
-	@Override
-	public SoundThread clone(){
-		System.out.println("Played Clip!");
-		return new SoundThread(clip);
 	}
 }
